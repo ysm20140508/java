5a3b321b201aac374b9ad6defd4ac4a4c0b046fb
shoumiao_yao <shoumiao_yao@kingdee.com>
2016-12-21 17:11:34 +0800

    disruptor的源码阅读心得

--- src/main/java/com/jxnu/disruptor/源码阅读.java
+++ src/main/java/com/jxnu/disruptor/源码阅读.java
@@ -2,8 +2,11 @@
  * disruptor高性能框架源码阅读的心得及体会
  *
  * 1: RingBuffer 在初始化时 直接根据factory生成event填充数组,减少new的实例
+ *
  * 2: Sequence   在控制序号生成时，直接使用java的UNSAVE(CAS机制)控制高并发
+ *
  * 3: ThreadInfo 当前线程的状态，threadMXBean.getThreadInfo(treadId)
+ *
  * 4: Sequencer 定序器获取next(n)源码解读
  *    //获取当前的序号
  *    long nextValue = this.nextValue;
@@ -30,7 +33,8 @@
       }
       //从新设置当前序号
       this.nextValue = nextSequence;
- * 5: EventProcessor 其实相当于一个```消费线程```,根据用户的对应的```等待策略从环中获取数据,然后把消费的数据交给```EventHandler```处理
+
+ * 5: EventProcessor 其实相当于一个消费线程,根据用户的对应的等待策略从环中获取数据,然后把消费的数据交给EventHandler处理
  *
  *
  *
